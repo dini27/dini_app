9acadf16f50223d094e41df862431f6f17b92994
dini <shettynandini@gmail.com>
2014-02-09 17:35:48 -0800

    First commit by Diniosss :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiniApp::Application.config.secret_key_base = '39dfda9924b6dc0b696760d8e9df7531454ce272dbf838defbdd99b67037ba317458b9026614a6d04a108d4c9f2e0f4d6830316e1e5504634c16e7c98774df44'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiniApp::Application.config.session_store :cookie_store, key: '_dini_app_session'
